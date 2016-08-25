6d3732657f5311f79487099cd5860ce1796d1e28
bocz.tivadar@lanoga.hu <bocz.tivadar@lanoga.hu>
2016-08-25 16:49:15 +0200

    #1 Implement Sudoku solver Implement base logic

---
+++ sudoku_solver/src/main/java/Main.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Tivadar Bocz on 2016.08.25..
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting board" + System.lineSeparator());
+        int[][] board = {
+                {0,0,8,0,7,9,0,0,0},
+                {6,0,0,0,5,4,0,3,0},
+                {4,0,0,0,0,0,0,6,0},
+                {0,7,0,8,0,0,0,0,3},
+                {1,0,0,0,0,0,0,0,8},
+                {9,0,0,0,0,1,0,4,0},
+                {0,2,0,0,0,0,0,0,7},
+                {0,1,0,9,8,0,0,0,6},
+                {0,0,0,3,1,0,9,0,0}
+        };
+        SudokuSolver.print(board);
+        System.out.println(System.lineSeparator());
+        if(SudokuSolver.Solve(board)) {
+            System.out.println("Found a solution"  + System.lineSeparator());
+            SudokuSolver.print(board);
+            System.out.println(System.lineSeparator());
+        }
+        else{
+            System.out.println("No solution is found" + System.lineSeparator());
+        }
+    }
+}
